d2b8f672c51b7b0e89f5b93486fd53cad6db58e1
ccpu <m.doaie@hotmail.com>
2026-02-27 13:45:43 +0000

    chore: remove unused ESLint and TypeScript configuration files
    * Deleted `eslint.config.mjs`, `config.ts`, and `tsconfig.json` as they are no longer needed.
    * Added `scaffoldfy.json` generator configuration for package generation.

--- turbo/generators/config.ts
+++
@@ -1,9 +0,0 @@
-import { packageGenerator } from '@pixpilot/workspace-package-generator';
-
-module.exports = function generator(plop: unknown) {
-  packageGenerator(plop, {
-    author: 'PixPilot <m.doaie@hotmail.com>',
-
-    orgName: 'pixpilot',
-  });
-};
